e0532797a221ed2f04900b8de19d3bdc1248abca
Hieumonki <hieumonki4@gmail.com>
2025-08-08 18:28:41 +0700

    Thêm API GET /v1/news/:id

--- model/new.js
+++ model/new.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const mongoosePaginate = require("mongoose-paginate-v2");
 
 const newsSchema = new mongoose.Schema({
-  title: { type: String, required: true, minlength: 6 },
-  description: String,
-  image: String,
-  createdAt: { type: Date, default: Date.now },
+  title: String,
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
-newsSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model("news", newsSchema);
+module.exports = mongoose.models.news || mongoose.model("news", newsSchema);
