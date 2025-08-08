6e8d2c1c606008e544abae63e4a8afb1e4d98a0b
Hieumonki <hieumonki4@gmail.com>
2025-08-08 18:24:40 +0700

    Thêm API GET /v1/news/:id

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { news } = require("../model/model");
+const { news } = require("../model/new");
 
 // GET tất cả bài viết tin tức
 router.get("/", async (req, res) => {
