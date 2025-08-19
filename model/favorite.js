d7b03f7fbf7ee3a36363ed76ee6eecf9d9f9d6b1
Hieumonki <hieumonki4@gmail.com>
2025-08-19 10:49:23 +0700

    g

--- model/favorite.js
+++ model/favorite.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const favoriteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   name: String,
   image: String,
   price: Number
