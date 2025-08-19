e0a41046b2ac3ea0e35532ae0ed61d5e7606ec11
Hieumonki <hieumonki4@gmail.com>
2025-08-19 14:22:05 +0700

    k

--- model/favorite.js
+++ model/favorite.js
@@ -1,8 +1,12 @@
-const mongoose = require("mongoose");
+// models/Favorite.js
+const mongoose = require('mongoose');
 
-const favoriteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }
-});
+const favoriteSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Favorite", favoriteSchema);
+module.exports = mongoose.model('Favorite', favoriteSchema);
--- routes/favorite.js
+++ routes/favorite.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { addFavorite, getFavoritesByUser, removeFavorite } = require("../controllers/favorite");
-const { varifyToken } = require(".././controllers/middlewareCon");
+const { varifyToken } = require("../controllers/middlewareCon");
 
 // User phải đăng nhập mới dùng được
 router.post("/", varifyToken, addFavorite);
