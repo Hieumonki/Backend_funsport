75305df101a7bed7c746614ce074e2e0af9fe45f
Hieumonki <hieumonki4@gmail.com>
2025-09-05 23:41:42 +0700

    f

--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const { addToCart, getCart, removeFromCart } = require("../controllers/cart");
-const auth = require("../controllers/middlewareCon"); // middleware xác thực token
+const { varifyToken } = require("../controllers/middlewareCon"); // ✅ destructure
 
-// các route cần login
-router.post("/add", auth, addToCart);
-router.get("/", auth, getCart);
-router.post("/remove", auth, removeFromCart);
+router.post("/add", varifyToken, addToCart);
+router.get("/", varifyToken, getCart);
+router.post("/remove", varifyToken, removeFromCart);
 
 module.exports = router;
