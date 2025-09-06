4294285641f9ca2b6f8ab237bb257ac8474241b4
Hieumonki <hieumonki4@gmail.com>
2025-09-06 11:01:33 +0700

    f

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { addToCart, getCart, removeFromCart } = require("../controllers/cart");
-const { varifyToken } = require("../controllers/middlewareConza"); // ✅ destructure
+const { varifyToken } = require("../controllers/middlewareCon"); // ✅ destructure
 
 router.post("/add", varifyToken, addToCart);
 router.get("/", varifyToken, getCart);
